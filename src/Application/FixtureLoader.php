30f9423e6637157a3b8cd17a0347dbe2f9352d7e
Marius Klocke <marius.klocke@eventim.de>
2017-12-25 14:27:50 +0100

    Simplified FixtureLoader

--- src/Application/FixtureLoader.php
+++ src/Application/FixtureLoader.php
@@ -14,26 +14,6 @@ class FixtureLoader
     /** @var UuidGeneratorInterface */
     private $uuidGenerator;
 
-    private static $teamNames = [
-        "BSG Rotation Gastfeld",
-        "Stümper 02",
-        "Zollhaus",
-        "Vibrator Moskovskaya",
-        "Interruptus Connection",
-        "Team Erasmus",
-        "Höttges' Erben",
-        "Mahatma G Punkt",
-        "Blaues Wunder Bremen",
-        "Azzurri di Brema",
-        "Hansa Gönnung",
-        "Hemelinger Helden",
-        "Mahatma Gondi",
-        "Harpune Poseidon",
-        "RB Rockstars",
-        "FC Talentfrei",
-        "Die elo.minaten"
-    ];
-
     public function __construct(ObjectPersistenceInterface $persistence, UuidGeneratorInterface $uuidGenerator)
     {
         $this->persistence = $persistence;
@@ -44,16 +24,12 @@ class FixtureLoader
     {
         $this->persistence->transactional(function() {
             $pitch = new Pitch($this->uuidGenerator, 'Sportgarten', new GeographicLocation('12.34', '23.45'));
-            $this->persist($pitch);
-            foreach (static::$teamNames as $teamName) {
-                $this->persist(new Team($this->uuidGenerator, $teamName));
+            $this->persistence->persist($pitch);
+            for ($i = 1; $i <= 18; $i++) {
+                $teamName = sprintf('Team No. %02d', $i);
+                $this->persistence->persist(new Team($this->uuidGenerator, $teamName));
             }
-            $this->persist(new Season($this->uuidGenerator, 'Winterliga 17/18'));
+            $this->persistence->persist(new Season($this->uuidGenerator, 'Winterliga 17/18'));
         });
-    }
-
-    private function persist($entity)
-    {
-        return $this->persistence->persist($entity);
     }
 }
