680a54312c6c3e631e1ec3afa4bbb02e66b4d577
Marius Klocke <marius.klocke@eventim.de>
2017-12-30 18:25:57 +0100

    Added public index.php for handling HTTP requests

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+// For PHP's internal webserver
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
+$app = require __DIR__ . '/../src/bootstrap.php';
+$app->run();
