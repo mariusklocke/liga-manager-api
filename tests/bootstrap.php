2a4ecf8068e473dc7e95f04d1b9fbfd6a2a31084
Marius Klocke <marius.klocke@gmail.com>
2018-02-10 21:42:33 +0100

    Add strict type declaration

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 /** @var \Composer\Autoload\ClassLoader $autoloader */
 $autoloader = require __DIR__ . '/../vendor/autoload.php';
 $autoloader->addPsr4('HexagonalPlayground\\Tests\\', __DIR__);
