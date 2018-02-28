fdae4dfaeabc1c2996255ea74c0e9a4d982644ad
Marius Klocke <marius.klocke@gmail.com>
2018-02-28 19:39:48 +0100

    Add strict_type declaration

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 $app = require __DIR__ . '/../src/bootstrap.php';
 $container = $app->getContainer();
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($container['doctrine.entityManager']);
