9e561bd2076292e115422eafca0cac31146d47c8
Marius Klocke <marius.klocke@gmail.com>
2018-03-05 22:53:50 +0100

    Remove sqlite testing stuff

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,8 +5,4 @@ declare(strict_types=1);
 $autoloader = require_once __DIR__ . '/../vendor/autoload.php';
 $autoloader->addPsr4('HexagonalPlayground\\Tests\\', __DIR__);
 
-putenv('SQLITE_PATH=' . tempnam(sys_get_temp_dir(), 'sqlite'));
 putenv('LOG_LEVEL=warning');
-register_shutdown_function(function() {
-    unlink(getenv('SQLITE_PATH'));
-});
