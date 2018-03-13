81f4b96223e161dc39f0cf316a2648096cee2417
Marius Klocke <marius.klocke@gmail.com>
2018-03-13 19:38:51 +0100

    Remove unnecessary code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,5 +4,3 @@ declare(strict_types=1);
 /** @var \Composer\Autoload\ClassLoader $autoloader */
 $autoloader = require_once __DIR__ . '/../vendor/autoload.php';
 $autoloader->addPsr4('HexagonalPlayground\\Tests\\', __DIR__);
-
-putenv('LOG_LEVEL=warning');
