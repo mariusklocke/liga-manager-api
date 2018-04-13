42325c336763ffa838d1f4b3e3e1f8454cb3156c
Marius Klocke <marius.klocke@gmail.com>
2018-04-13 22:55:25 +0200

    Fix run-tests.sh returning proper exit codes by using trap

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -6,8 +6,10 @@ fi
 if [[ -z "${JWT_SECRET_PATH}" ]]; then
   JWT_SECRET_PATH="/etc/jwt"
 fi
+trap 'rc=$?' ERR
 docker run -d --name mariadb --env-file .env mariadb > /dev/null
 docker run --link mariadb --rm dadarek/wait-for-dependencies mariadb:3306
 docker run --link mariadb --env-file .env --rm -v $JWT_SECRET_PATH:/etc/jwt mklocke/liga-manager-api:$TAG sh -c "bin/console.php orm:schema-tool:create && bin/console.php app:load-fixtures && phpunit"
 docker stop mariadb > /dev/null
 docker rm mariadb > /dev/null
+exit ${rc}
