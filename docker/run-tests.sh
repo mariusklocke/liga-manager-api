f7f1218daf022e9e42db0d67b7804f3ce317fba5
Marius Klocke <marius.klocke@gmail.com>
2018-04-13 22:18:29 +0200

    Prepare CI config to work with tags

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+if [[ -z "${TAG}" ]]; then
+  TAG="latest"
+fi
+if [[ -z "${JWT_SECRET_PATH}" ]]; then
+  JWT_SECRET_PATH="/etc/jwt"
+fi
 docker run -d --name mariadb --env-file .env mariadb > /dev/null
-docker run --link mariadb dadarek/wait-for-dependencies mariadb:3306
-docker run --link mariadb --env-file .env --rm -v $JWT_SECRET_PATH:/etc/jwt mklocke/liga-manager-api bin/console.php orm:schema-tool:create
-docker run --link mariadb --env-file .env --rm -v $JWT_SECRET_PATH:/etc/jwt mklocke/liga-manager-api bin/console.php app:load-fixtures
-docker run --link mariadb --env-file .env --rm -v $JWT_SECRET_PATH:/etc/jwt mklocke/liga-manager-api phpunit
+docker run --link mariadb --rm dadarek/wait-for-dependencies mariadb:3306
+docker run --link mariadb --env-file .env --rm -v $JWT_SECRET_PATH:/etc/jwt mklocke/liga-manager-api:$TAG sh -c "bin/console.php orm:schema-tool:create && bin/console.php app:load-fixtures && phpunit"
 docker stop mariadb > /dev/null
 docker rm mariadb > /dev/null
