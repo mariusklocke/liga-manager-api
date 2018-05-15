6ab23c07414212c985241959e39b14d311ea6317
Marius Klocke <marius.klocke@gmail.com>
2018-05-15 16:43:59 +0200

    Strengthen test environment

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -7,11 +7,16 @@ if [[ -z "${JWT_SECRET_PATH}" ]]; then
   JWT_SECRET_PATH="/etc/jwt"
 fi
 trap 'rc=$?' ERR
-docker run -d --name mariadb --env-file .env mariadb > /dev/null
+docker run -d --name mariadb \
+    -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -e MYSQL_DATABASE=test -e MYSQL_USER=test -e MYSQL_PASSWORD=test \
+    mariadb > /dev/null
 docker run -d --name mongo mongo > /dev/null
 docker run --link mariadb --rm dadarek/wait-for-dependencies mariadb:3306
 docker run --link mongo --rm dadarek/wait-for-dependencies mongo:27017
-docker run --link mariadb --link mongo --env-file .env --rm mklocke/liga-manager-api:$TAG sh -c "bin/generate-jwt-key.sh && bin/console.php orm:schema-tool:create && bin/console.php app:load-fixtures && bin/console.php orm:generate-proxies && phpunit"
+docker run --link mariadb --link mongo --rm \
+    -e MYSQL_HOST=mariadb -e MYSQL_DATABASE=test -e MYSQL_USER=test -e MYSQL_PASSWORD=test \
+    -e MONGO_HOST=mongo -e MONGO_DATABASE=test \
+    mklocke/liga-manager-api:$TAG sh -c "bin/generate-jwt-key.sh && bin/console.php orm:schema-tool:create && bin/console.php app:load-fixtures && bin/console.php orm:generate-proxies && phpunit"
 docker stop mariadb mongo > /dev/null
 docker rm mariadb mongo > /dev/null
 exit ${rc}
