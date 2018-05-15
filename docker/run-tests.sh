3eeadc279b24eb1ee007fe50faa245aa5f829987
Marius Klocke <marius.klocke@gmail.com>
2018-05-15 16:18:02 +0200

    Fix tests

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -11,7 +11,7 @@ docker run -d --name mariadb --env-file .env mariadb > /dev/null
 docker run -d --name mongo mongo > /dev/null
 docker run --link mariadb --rm dadarek/wait-for-dependencies mariadb:3306
 docker run --link mongo --rm dadarek/wait-for-dependencies mongo:27017
-docker run --link mariadb --link mongo --env-file .env --rm mklocke/liga-manager-api:$TAG sh -c "bin/generate-jwt-key.sh && bin/console.php orm:schema-tool:create && bin/console.php app:load-fixtures && phpunit"
+docker run --link mariadb --link mongo --env-file .env --rm mklocke/liga-manager-api:$TAG sh -c "bin/generate-jwt-key.sh && bin/console.php orm:schema-tool:create && bin/console.php app:load-fixtures && bin/console.php orm:generate-proxies && phpunit"
 docker stop mariadb mongo > /dev/null
 docker rm mariadb mongo > /dev/null
 exit ${rc}
