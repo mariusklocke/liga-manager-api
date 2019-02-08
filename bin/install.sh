acd6548f1b6bb35e3338dc609e4b17ed540d6cac
Marius Klocke <marius.klocke@gmail.com>
2019-02-08 21:34:40 +0100

    Remove dependency to openssh-keygen

--- bin/install.sh
+++ bin/install.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 set -ex
 
-if ssh-keygen -t rsa -b 4096 -f ${JWT_SECRET_PATH}/secret.key -N ''; then
-    rm ${JWT_SECRET_PATH}/secret.key.pub
-    chmod 600 ${JWT_SECRET_PATH}/secret.key
-fi
-
 php vendor/bin/doctrine orm:schema-tool:drop --force
 php vendor/bin/doctrine orm:schema-tool:create
 php vendor/bin/doctrine orm:generate-proxies
+php bin/console.php app:generate-jwt-secret
 php bin/console.php app:load-fixtures
