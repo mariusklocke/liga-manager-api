2da814193d7d4c04d215791f3ae5241a2735034f
Marius Klocke <marius.klocke@gmail.com>
2019-02-24 21:15:50 +0100

    Update versions

--- bin/installPharTools.php
+++ bin/installPharTools.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 $tools = [
-    "https://getcomposer.org/download/1.6.3/composer.phar" => __DIR__ . '/../composer.phar',
-    "https://phar.phpunit.de/phpunit-7.0.2.phar" => __DIR__ . '/../phpunit.phar',
+    "https://getcomposer.org/composer.phar" => __DIR__ . '/../composer.phar',
+    "https://phar.phpunit.de/phpunit-8.0.4.phar" => __DIR__ . '/../phpunit.phar.bak',
     "https://raw.github.com/mamuz/PhpDependencyAnalysis/master/download/phpda.pubkey" => __DIR__ . '/../phpda.phar.pubkey',
     "https://raw.github.com/mamuz/PhpDependencyAnalysis/master/download/phpda" => __DIR__ . '/../phpda.phar'
 ];
