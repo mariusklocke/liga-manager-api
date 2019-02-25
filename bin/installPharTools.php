ec2c12f60f4422f3ddcc1c51dfbd1c5d40e43688
Marius Klocke <marius.klocke@gmail.com>
2019-02-25 19:12:42 +0100

    Fix filename

--- bin/installPharTools.php
+++ bin/installPharTools.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 $tools = [
     "https://getcomposer.org/composer.phar" => __DIR__ . '/../composer.phar',
-    "https://phar.phpunit.de/phpunit-8.0.4.phar" => __DIR__ . '/../phpunit.phar.bak',
+    "https://phar.phpunit.de/phpunit-8.0.4.phar" => __DIR__ . '/../phpunit.phar',
     "https://raw.github.com/mamuz/PhpDependencyAnalysis/master/download/phpda.pubkey" => __DIR__ . '/../phpda.phar.pubkey',
     "https://raw.github.com/mamuz/PhpDependencyAnalysis/master/download/phpda" => __DIR__ . '/../phpda.phar'
 ];
