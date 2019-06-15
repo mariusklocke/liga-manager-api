f25fb0dabe8ee8200957e46e679954a927e33930
Marius Klocke <marius.klocke@gmail.com>
2019-06-15 20:41:41 +0200

    Remove support for REST API

--- src/Infrastructure/API/Routing/RouteProvider.php
+++ src/Infrastructure/API/Routing/RouteProvider.php
@@ -4,30 +4,11 @@ declare(strict_types=1);
 
 namespace HexagonalPlayground\Infrastructure\API\Routing;
 
-use HexagonalPlayground\Infrastructure\API\Controller\EventQueryController;
-use HexagonalPlayground\Infrastructure\API\Controller\MatchCommandController;
-use HexagonalPlayground\Infrastructure\API\Controller\MatchDayCommandController;
-use HexagonalPlayground\Infrastructure\API\Controller\MatchQueryController;
-use HexagonalPlayground\Infrastructure\API\Controller\PitchCommandController;
-use HexagonalPlayground\Infrastructure\API\Controller\PitchQueryController;
-use HexagonalPlayground\Infrastructure\API\Controller\SeasonCommandController;
-use HexagonalPlayground\Infrastructure\API\Controller\SeasonQueryController;
-use HexagonalPlayground\Infrastructure\API\Controller\TeamCommandController;
-use HexagonalPlayground\Infrastructure\API\Controller\TeamQueryController;
-use HexagonalPlayground\Infrastructure\API\Controller\TournamentCommandController;
-use HexagonalPlayground\Infrastructure\API\Controller\TournamentQueryController;
-use HexagonalPlayground\Infrastructure\API\Controller\UserCommandController;
-use HexagonalPlayground\Infrastructure\API\Controller\UserQueryController;
 use HexagonalPlayground\Infrastructure\API\GraphQL\Controller as GraphQLController;
 use HexagonalPlayground\Infrastructure\API\Security\AuthenticationMiddleware;
-use HexagonalPlayground\Infrastructure\Persistence\Read\EventRepository;
-use HexagonalPlayground\Infrastructure\Persistence\Read\MatchRepository;
-use HexagonalPlayground\Infrastructure\Persistence\Read\PitchRepository;
-use HexagonalPlayground\Infrastructure\Persistence\Read\SeasonRepository;
-use HexagonalPlayground\Infrastructure\Persistence\Read\TeamRepository;
-use HexagonalPlayground\Infrastructure\Persistence\Read\TournamentRepository;
-use HexagonalPlayground\Infrastructure\Persistence\Read\UserRepository;
 use Slim\App;
+use Slim\Http\Response;
+use Slim\Http\StatusCode;
 
 class RouteProvider
 {
@@ -38,193 +19,187 @@ class RouteProvider
             $auth      = new AuthenticationMiddleware($container);
 
             $app->get('/teams', function () use ($container) {
-                return (new TeamQueryController($container[TeamRepository::class]))->findAllTeams();
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->get('/teams/{id}', function ($request, $response, $args) use ($container) {
-                return (new TeamQueryController($container[TeamRepository::class]))->findTeamById($args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->put('/teams/{id}/contact', function ($request, $response, $args) use ($container) {
-                return (new TeamCommandController($container['commandBus']))->updateContact($args['id'], $request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->put('/teams/{id}/name', function ($request, $response, $args) use ($container) {
-                return (new TeamCommandController($container['commandBus']))->rename($args['id'], $request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->get('/seasons/{id}/teams', function ($request, $response, $args) use ($container) {
-                return (new TeamQueryController($container[TeamRepository::class]))->findTeamsBySeasonId($args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->post('/teams', function ($request) use ($container) {
-                return (new TeamCommandController($container['commandBus']))->create($request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->delete('/teams/{id}', function ($request, $response, $args) use ($container) {
-                return (new TeamCommandController($container['commandBus']))->delete($request, $args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->get('/seasons', function () use ($container) {
-                return (new SeasonQueryController($container[SeasonRepository::class]))->findAllSeasons();
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->get('/seasons/{id}', function ($request, $response, $args) use ($container) {
-                return (new SeasonQueryController($container[SeasonRepository::class]))->findSeasonById($args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->get('/seasons/{id}/ranking', function ($request, $response, $args) use ($container) {
-                return (new SeasonQueryController($container[SeasonRepository::class]))->findRanking($args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->post('/seasons/{season_id}/ranking/penalties', function ($request, $response, $args) use ($container) {
-                return (new SeasonCommandController($container['commandBus']))
-                    ->addRankingPenalty($request, $args['season_id']);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->delete('/seasons/{season_id}/ranking/penalties/{penalty_id}', function ($request, $response, $args) use ($container) {
-                return (new SeasonCommandController($container['commandBus']))
-                    ->removeRankingPenalty($request, $args['season_id'], $args['penalty_id']);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->get('/matches', function ($request) use ($container) {
-                return (new MatchQueryController($container[MatchRepository::class]))->findMatches($request);
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->get('/matches/{id}', function ($request, $response, $args) use ($container) {
-                return (new MatchQueryController($container[MatchRepository::class]))->findMatchById($args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->get('/pitches', function () use ($container) {
-                return (new PitchQueryController($container[PitchRepository::class]))->findAllPitches();
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->get('/pitches/{id}', function ($request, $response, $args) use ($container) {
-                return (new PitchQueryController($container[PitchRepository::class]))->findPitchById($args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->post('/pitches', function ($request) use ($container) {
-                return (new PitchCommandController($container['commandBus']))->create($request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->put('/pitches/{id}/contact', function ($request, $response, $args) use ($container) {
-                return (new PitchCommandController($container['commandBus']))->updateContact($args['id'], $request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->post('/seasons/{id}/start', function ($request, $response, $args) use ($container) {
-                return (new SeasonCommandController($container['commandBus']))->start($request, $args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->post('/seasons/{id}/end', function ($request, $response, $args) use ($container) {
-                return (new SeasonCommandController($container['commandBus']))->end($request, $args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->delete('/seasons/{id}', function ($request, $response, $args) use ($container) {
-                return (new SeasonCommandController($container['commandBus']))->delete($request, $args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->get('/seasons/{id}/match_days', function ($request, $response, $args) use ($container) {
-                return (new SeasonQueryController($container[SeasonRepository::class]))->findMatchDays($args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->post('/seasons/{id}/match_days', function ($request, $response, $args) use ($container) {
-                return (new SeasonCommandController($container['commandBus']))->createMatches($args['id'], $request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->post('/matches/{id}/kickoff', function ($request, $response, $args) use ($container) {
-                return (new MatchCommandController($container['commandBus']))->schedule($args['id'], $request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->post('/matches/{id}/location', function ($request, $response, $args) use ($container) {
-                return (new MatchCommandController($container['commandBus']))->locate($args['id'], $request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->post('/matches/{id}/result', function ($request, $response, $args) use ($container) {
-                return (new MatchCommandController($container['commandBus']))->submitResult($args['id'], $request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->post('/matches/{id}/cancellation', function ($request, $response, $args) use ($container) {
-                return (new MatchCommandController($container['commandBus']))->cancel($request, $args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->put('/seasons/{season_id}/teams/{team_id}', function ($request, $response, $args) use ($container) {
-                return (new SeasonCommandController($container['commandBus']))->addTeam($request, $args['season_id'], $args['team_id']);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->delete('/seasons/{season_id}/teams/{team_id}', function ($request, $response, $args) use ($container) {
-                return (new SeasonCommandController($container['commandBus']))->removeTeam($request, $args['season_id'], $args['team_id']);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->post('/seasons', function ($request) use ($container) {
-                return (new SeasonCommandController($container['commandBus']))->createSeason($request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->post('/tournaments', function ($request) use ($container) {
-                return (new TournamentCommandController($container['commandBus']))->create($request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->get('/tournaments/{id}/rounds', function ($request, $response, $args) use ($container) {
-                return (new TournamentQueryController($container[TournamentRepository::class]))
-                    ->findRounds($args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->put('/tournaments/{id}/rounds/{round}', function ($request, $response, $args) use ($container) {
-                return (new TournamentCommandController($container['commandBus']))->setRound($args['id'], (int) $args['round'], $request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->get('/tournaments', function () use ($container) {
-                return (new TournamentQueryController($container[TournamentRepository::class]))->findAllTournaments();
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->get('/tournaments/{id}', function ($request, $response, $args) use ($container) {
-                return (new TournamentQueryController($container[TournamentRepository::class]))->findTournamentById($args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->delete('/tournaments/{id}', function ($request, $response, $args) use ($container) {
-                return (new TournamentCommandController($container['commandBus']))->delete($request, $args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->get('/users/me', function ($request) use ($container) {
-                return (new UserQueryController($container[UserRepository::class]))->getAuthenticatedUser($request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->get('/users', function ($request) use ($container) {
-                return (new UserQueryController($container[UserRepository::class]))->findAllUsers($request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->put('/users/me/password', function ($request) use ($container) {
-                return (new UserCommandController($container['commandBus']))->changePassword($request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->post('/users', function ($request) use ($container) {
-                return (new UserCommandController($container['commandBus']))->createUser($request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->delete('/users/{id}', function ($request, $response, $args) use ($container) {
-                return (new UserCommandController($container['commandBus']))->deleteUser($request, $args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->patch('/users/{id}', function ($request, $response, $args) use ($container) {
-                return (new UserCommandController($container['commandBus']))->updateUser($args['id'], $request);
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->post('/users/me/password/reset', function ($request) use ($container) {
-                return (new UserCommandController($container['commandBus']))->sendPasswordResetMail($request);
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->patch('/match_days/{id}', function ($request, $response, $args) use ($container) {
-                return (new MatchDayCommandController($container['commandBus']))->rescheduleMatchDay(
-                    $args['id'],
-                    $request
-                );
+                return new Response(StatusCode::HTTP_GONE);
             })->add($auth);
 
             $app->get('/events', function ($request, $response, $args) use ($container) {
-                return (new EventQueryController($container[EventRepository::class]))->findLatestEvents($request);
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->get('/events/{id}', function ($request, $response, $args) use ($container) {
-                return (new EventQueryController($container[EventRepository::class]))->findEventById($args['id']);
+                return new Response(StatusCode::HTTP_GONE);
             });
 
             $app->post('/graphql', function ($request, $response, $args) use ($container) {
