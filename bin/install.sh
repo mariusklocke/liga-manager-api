55dc8fb36a510e6264609f879e87e72f8aef6e5b
Marius Klocke <marius.klocke@gmail.com>
2019-10-16 22:31:27 +0200

    Use doctrine migrations in installer

--- bin/install.sh
+++ bin/install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -ex
 
-php vendor/bin/doctrine orm:schema-tool:drop --force
-php vendor/bin/doctrine orm:schema-tool:create
 php vendor/bin/doctrine orm:generate-proxies
+php vendor/bin/doctrine-migrations migrations:migrate -n
 
 if [ "$ADMIN_EMAIL " != "" ] && [ "$ADMIN_PASSWORD" != "" ]; then
     php bin/console.php app:create-user --email=$ADMIN_EMAIL --password=$ADMIN_PASSWORD
