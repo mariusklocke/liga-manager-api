b02bf472b45c670c295c32ba9f16daa54833aedc
Marius Klocke <marius.klocke@gmail.com>
2019-10-16 23:35:12 +0200

    Fix build

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -ex
 
+php vendor/bin/doctrine orm:schema-tool:drop --force
+php vendor/bin/doctrine dbal:run-sql "DROP TABLE IF EXISTS doctrine_migration_versions;"
 php vendor/bin/doctrine-migrations migrations:migrate -n
 
 if [ "$ADMIN_EMAIL " != "" ] && [ "$ADMIN_PASSWORD" != "" ]; then
