ca3e7946ffa6c182f12f1da2d050ef756b451fb7
Marius Klocke <marius.klocke@gmail.com>
2019-10-26 21:19:45 +0200

    Improve tests

--- tests/WebAuthn/AuthTest.php
+++ tests/WebAuthn/AuthTest.php
@@ -2,6 +2,7 @@
 
 namespace HexagonalPlayground\Tests\WebAuthn;
 
+use HexagonalPlayground\Infrastructure\Environment;
 use HexagonalPlayground\Tests\Framework\HttpTest;
 
 class AuthTest extends HttpTest
@@ -23,5 +24,22 @@ class AuthTest extends HttpTest
         self::assertObjectHasAttribute('user', $options);
         self::assertIsObject($options->user);
         self::assertObjectHasAttribute('id', $options->user);
+    }
+
+    public function testLoginOptionsCanBeRequested(): void
+    {
+        $request = $this->createRequest(
+            'POST',
+            '/api/webauthn/login/options',
+            ['email' => Environment::get('ADMIN_EMAIL')]
+        );
+
+        $response = $this->client->sendRequest($request);
+        self::assertSame(200, $response->getStatusCode());
+
+        $options = $this->parser->parse($response);
+        self::assertIsObject($options);
+        self::assertObjectHasAttribute('challenge', $options);
+        self::assertObjectHasAttribute('rpId', $options);
     }
 }
