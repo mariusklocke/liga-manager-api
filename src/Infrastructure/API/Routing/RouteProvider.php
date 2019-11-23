82f121b7a88739296947335ef544515abde49ae4
Marius Klocke <marius.klocke@gmail.com>
2019-11-23 14:29:12 +0100

    Fix hidden dependency to ArrayAccess for $container

--- src/Infrastructure/API/Routing/RouteProvider.php
+++ src/Infrastructure/API/Routing/RouteProvider.php
@@ -24,64 +24,64 @@ class RouteProvider
 
             $app->get('/webauthn/test', function () use ($container) {
                 /** @var TestClientController $testClientController */
-                $testClientController = $container[TestClientController::class];
+                $testClientController = $container->get(TestClientController::class);
 
                 return $testClientController->show();
             });
 
             $app->post('/webauthn/credential/options', function ($request) use ($container) {
                 /** @var CredentialController $credentialController */
-                $credentialController = $container[CredentialController::class];
+                $credentialController = $container->get(CredentialController::class);
 
                 return $credentialController->options($request);
             })->add($auth);
 
             $app->post('/webauthn/credential', function ($request) use ($container) {
                 /** @var CredentialController $credentialController */
-                $credentialController = $container[CredentialController::class];
+                $credentialController = $container->get(CredentialController::class);
 
                 return $credentialController->create($request);
             })->add($auth);
 
             $app->get('/webauthn/credential', function ($request) use ($container) {
                 /** @var CredentialController $credentialController */
-                $credentialController = $container[CredentialController::class];
+                $credentialController = $container->get(CredentialController::class);
 
                 return $credentialController->findAll($request);
             })->add($auth);
 
             $app->delete('/webauthn/credential', function ($request) use ($container) {
                 /** @var CredentialController $credentialController */
-                $credentialController = $container[CredentialController::class];
+                $credentialController = $container->get(CredentialController::class);
 
                 return $credentialController->deleteAll($request);
             })->add($auth);
 
             $app->delete('/webauthn/credential/{id}', function ($request, $response, $args) use ($container) {
                 /** @var CredentialController $credentialController */
-                $credentialController = $container[CredentialController::class];
+                $credentialController = $container->get(CredentialController::class);
 
                 return $credentialController->deleteOne($request, $args['id']);
             })->add($auth);
 
             $app->post('/webauthn/login/options', function ($request) use ($container) {
                 /** @var AuthController $authController */
-                $authController = $container[AuthController::class];
+                $authController = $container->get(AuthController::class);
 
                 return $authController->options($request);
             });
 
             $app->post('/webauthn/login', function ($request) use ($container) {
                 /** @var AuthController $authController */
-                $authController = $container[AuthController::class];
+                $authController = $container->get(AuthController::class);
 
                 return $authController->login($request);
             });
 
             $app->post('/graphql', function ($request, $response, $args) use ($container) {
                 $appContext = new AppContext($request, $container);
-                $errorHandler = new ErrorHandler($container['logger'], $appContext);
-                $controller = new GraphQLController($appContext, $container[Schema::class], $errorHandler);
+                $errorHandler = new ErrorHandler($container->get('logger'), $appContext);
+                $controller = new GraphQLController($appContext, $container->get(Schema::class), $errorHandler);
 
                 return $controller->query($request);
             })->add($auth);
