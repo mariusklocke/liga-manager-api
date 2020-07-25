26c0219bb397c5d5702194dc3b8895051e7684e9
Marius Klocke <marius.klocke@gmail.com>
2020-07-25 18:23:59 +0200

    Improve tls cert stuff

--- docker/nginx/generate-site-cert.sh
+++ docker/nginx/generate-site-cert.sh
@@ -6,16 +6,20 @@ fi
 
 ROOT_CA_CERT=/etc/ssl/certs/local-ca.crt
 ROOT_CA_KEY=/etc/ssl/private/local-ca.key
+SITE_CERT=/etc/ssl/certs/lima.local.crt
+SITE_CSR=/etc/ssl/certs/lima.local.csr
+SITE_KEY=/etc/ssl/private/lima.local.key
+SITE_EXT=docker/nginx/local.ext
 
 openssl req -new -nodes -newkey rsa:2048 \
-    -subj "/C=DE/O=localhost/CN=localhost" \
-    -out /etc/ssl/certs/localhost.csr \
-    -keyout /etc/ssl/private/localhost.key
+    -subj "/C=DE/O=lima.local/CN=lima.local" \
+    -out ${SITE_CSR} \
+    -keyout ${SITE_KEY}
 
 openssl x509 -req -days 3650 -sha256 \
-    -in /etc/ssl/certs/localhost.csr \
+    -in ${SITE_CSR} \
     -CA ${ROOT_CA_CERT} \
     -CAkey ${ROOT_CA_KEY} \
     -CAcreateserial \
-    -out /etc/ssl/certs/localhost.crt \
-    -extfile localhost.ext
+    -out ${SITE_CERT} \
+    -extfile ${SITE_EXT}
