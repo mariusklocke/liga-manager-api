45db94321f80befde0011c79293c83a9c298a0f6
Marius Klocke <marius.klocke@gmail.com>
2022-01-26 18:50:13 +0100

    Fix migration

--- migrations/Version20220126171550.php
+++ migrations/Version20220126171550.php
@@ -7,19 +7,16 @@ namespace Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Clear invalid comment for DBAL 3 compatibility
- */
 final class Version20220126171550 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Update comment for compatibility with Doctrine DBAL 3.x';
     }
 
     public function up(Schema $schema): void
     {
-        $this->addSql("ALTER TABLE `public_key_credentials` CHANGE `transports` `transports` LONGTEXT COMMENT '';");
+        $this->addSql("ALTER TABLE `public_key_credentials` CHANGE `transports` `transports` LONGTEXT COMMENT '(DC2Type:json)';");
     }
 
     public function down(Schema $schema): void
