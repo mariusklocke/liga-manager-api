f1defbc6795fa12a800f63278b3a22da0080490e
Marius Klocke <marius.klocke@gmail.com>
2022-05-08 16:38:53 +0200

    Increase retries for initial health checks

--- docker/php/init-container.sh
+++ docker/php/init-container.sh
@@ -2,6 +2,6 @@
 set -e
 
 lima app:maintenance --mode=on
-lima app:health --skip=fpm --retries=10
+lima app:health --skip=fpm --retries=20
 lima migrations:migrate -n
 lima app:maintenance --mode=off
