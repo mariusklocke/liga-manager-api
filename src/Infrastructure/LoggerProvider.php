4384de8fa7fd62aa4b8d8fe18d920e1ee9939fdc
Marius Klocke <marius.klocke@gmail.com>
2022-07-26 19:45:41 +0200

    Fix issue with undefined constant in FPM context

--- src/Infrastructure/LoggerProvider.php
+++ src/Infrastructure/LoggerProvider.php
@@ -13,7 +13,7 @@ class LoggerProvider implements ServiceProviderInterface
     {
         return [
             LoggerInterface::class => DI\factory(function () {
-                Logger::init(STDOUT, getenv('LOG_LEVEL') ?: 'notice');
+                Logger::init(fopen('php://stdout', 'w'), getenv('LOG_LEVEL') ?: 'notice');
 
                 return Logger::getInstance();
             })
