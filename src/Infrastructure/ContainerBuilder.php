f5cffdf420672ff78589dabf95be8cd5e1619ec1
Marius Klocke <marius.klocke@gmail.com>
2023-12-11 23:04:14 +0100

    Brand version into source code

--- src/Infrastructure/ContainerBuilder.php
+++ src/Infrastructure/ContainerBuilder.php
@@ -14,11 +14,8 @@ class ContainerBuilder
      */
     public static function build(array $serviceProviders): ContainerInterface
     {
-        $versionFilePath = join(DIRECTORY_SEPARATOR, [getenv('APP_HOME'), 'VERSION']);
-
         $builder = new DI\ContainerBuilder();
         $builder->useAutowiring(true);
-        $builder->addDefinitions(['version' => @file_get_contents($versionFilePath) ?: 'development']);
 
         foreach ($serviceProviders as $provider) {
             $builder->addDefinitions($provider->getDefinitions());
