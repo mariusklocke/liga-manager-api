4738e09cac1b8d96f537cabf149475591039db3b
Marius Klocke <marius.klocke@gmail.com>
2024-08-28 23:47:17 +0200

    Add more missing extension

--- docker/php/roadrunner/packages.sh
+++ docker/php/roadrunner/packages.sh
@@ -3,6 +3,22 @@ set -xe
 
 apk update
 apk upgrade
-apk add --no-cache php83 php83-bcmath php83-dom php83-gmp php83-mbstring php83-opcache php83-pcntl php83-pdo_mysql php83-phar php83-simplexml php83-sockets
-apk add --no-cache php83-pecl-apcu php83-pecl-redis php83-pecl-xdebug
+apk add --no-cache \
+    php83 \
+    php83-bcmath \
+    php83-dom \
+    php83-gmp \
+    php83-mbstring \
+    php83-opcache \
+    php83-pcntl \
+    php83-pdo_mysql \
+    php83-phar \
+    php83-simplexml \
+    php83-sockets \
+    php83-tokenizer \
+    php83-xml \
+    php83-xmlwriter \
+    php83-pecl-apcu \
+    php83-pecl-redis \
+    php83-pecl-xdebug
 rm -rf /var/cache/*
