ee9cdd316c8057c2e85da15cbd0173c3283c719b
Marius Klocke <marius.klocke@gmail.com>
2024-08-29 00:39:12 +0200

    Refactor packages.sh

--- docker/php/fpm/packages.sh
+++ docker/php/fpm/packages.sh
@@ -1,26 +1,15 @@
 #!/bin/sh
-set -xe
 
+PHP_EXTENSIONS="apcu bcmath dom fpm gmp mbstring opcache pcntl pdo_mysql phar redis simplexml sockets tokenizer xdebug xml xmlwriter"
+PHP_MAIN_PACKAGE="php83"
+PACKAGES="fcgi ${PHP_MAIN_PACKAGE}"
+
+for PHP_EXTENSION in $PHP_EXTENSIONS; do
+    PACKAGES="${PACKAGES} ${PHP_MAIN_PACKAGE}-${PHP_EXTENSION}"
+done
+
+set -ex
 apk update
 apk upgrade
-apk add --no-cache \
-    fcgi \
-    php83 \
-    php83-apcu \
-    php83-bcmath \
-    php83-dom \
-    php83-fpm \
-    php83-gmp \
-    php83-mbstring \
-    php83-opcache \
-    php83-pcntl \
-    php83-pdo_mysql \
-    php83-phar \
-    php83-redis \
-    php83-simplexml \
-    php83-sockets \
-    php83-tokenizer \
-    php83-xdebug \
-    php83-xml \
-    php83-xmlwriter
+apk add --no-cache "${PACKAGES}"
 rm -rf /var/cache/*
