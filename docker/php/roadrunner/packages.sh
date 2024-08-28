86b11e2c1b9a514605d8d0398b092507a02f4fe0
Marius Klocke <marius.klocke@gmail.com>
2024-08-28 23:15:37 +0200

    Simplify packages.sh

--- docker/php/roadrunner/packages.sh
+++ docker/php/roadrunner/packages.sh
@@ -5,4 +5,4 @@ apk update
 apk upgrade
 apk add --no-cache php83 php83-bcmath php83-gmp php83-opcache php83-pcntl php83-pdo_mysql php83-phar php83-sockets
 apk add --no-cache php83-pecl-apcu php83-pecl-redis php83-pecl-xdebug
-rm -rf /tmp/* /var/cache/*
+rm -rf /var/cache/*
