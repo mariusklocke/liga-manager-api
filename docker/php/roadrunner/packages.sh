eab79a1c11c5a7e36b0eb910d8ad8d7e7eeb5d2c
Marius Klocke <marius.klocke@gmail.com>
2024-08-28 23:40:39 +0200

    Add missing "simplexml" extension

--- docker/php/roadrunner/packages.sh
+++ docker/php/roadrunner/packages.sh
@@ -3,6 +3,6 @@ set -xe
 
 apk update
 apk upgrade
-apk add --no-cache php83 php83-bcmath php83-gmp php83-mbstring php83-opcache php83-pcntl php83-pdo_mysql php83-phar php83-sockets
+apk add --no-cache php83 php83-bcmath php83-gmp php83-mbstring php83-opcache php83-pcntl php83-pdo_mysql php83-phar php83-simplexml php83-sockets
 apk add --no-cache php83-pecl-apcu php83-pecl-redis php83-pecl-xdebug
 rm -rf /var/cache/*
