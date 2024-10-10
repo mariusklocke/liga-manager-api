d53f536b20219965ac113e894a6abf5197f338d6
Marius Klocke <marius.klocke@gmail.com>
2024-10-10 22:59:56 +0200

    Fix FileSystemTest

--- tests/Unit/FileSystemTest.php
+++ tests/Unit/FileSystemTest.php
@@ -4,40 +4,47 @@ declare(strict_types=1);
 namespace HexagonalPlayground\Tests\Unit;
 
 use HexagonalPlayground\Infrastructure\Filesystem\FilesystemService;
+use Nyholm\Psr7\Factory\Psr17Factory;
 use PHPUnit\Framework\Attributes\Depends;
 use PHPUnit\Framework\TestCase;
 
 class FileSystemTest extends TestCase
 {
+    private FilesystemService $service;
+
+    protected function setUp(): void
+    {
+        $streamFactory = new Psr17Factory();
+        $this->service = new FilesystemService($streamFactory);
+    }
+
     public function testBasicFileOperations(): void
     {
-        $service = new FilesystemService();
-        self::assertTrue($service->isDirectory(sys_get_temp_dir()));
+        self::assertTrue($this->service->isDirectory(sys_get_temp_dir()));
 
-        $basePath = $service->joinPaths([sys_get_temp_dir(), uniqid('phpunit')]);
-        $service->createDirectory($basePath);
-        self::assertTrue($service->isDirectory($basePath));
-        self::assertCount(0, $service->getDirectoryContents($basePath));
+        $basePath = $this->service->joinPaths([sys_get_temp_dir(), uniqid('phpunit')]);
+        $this->service->createDirectory($basePath);
+        self::assertTrue($this->service->isDirectory($basePath));
+        self::assertCount(0, $this->service->getDirectoryContents($basePath));
 
-        $filePath = $service->joinPaths([$basePath, uniqid('phpunit')]);
+        $filePath = $this->service->joinPaths([$basePath, uniqid('phpunit')]);
         $fileContent = uniqid();
-        $service->createFile($filePath, $fileContent);
-        self::assertSame($fileContent, $service->getFileContents($filePath));
-        self::assertFalse($service->isDirectory($filePath));
-        self::assertCount(1, $service->getDirectoryContents($basePath));
+        $this->service->createFile($filePath, $fileContent);
+        self::assertSame($fileContent, $this->service->getFileContents($filePath));
+        self::assertFalse($this->service->isDirectory($filePath));
+        self::assertCount(1, $this->service->getDirectoryContents($basePath));
 
-        $service->deleteFile($filePath);
-        self::assertCount(0, $service->getDirectoryContents($basePath));
+        $this->service->deleteFile($filePath);
+        self::assertCount(0, $this->service->getDirectoryContents($basePath));
 
-        $service->deleteDirectory($basePath);
-        self::assertFalse($service->isDirectory($basePath));
+        $this->service->deleteDirectory($basePath);
+        self::assertFalse($this->service->isDirectory($basePath));
     }
 
     public function testFileCanBeWritten(): string
     {
         $tempFilePath = tempnam(sys_get_temp_dir(), uniqid('phpunit'));
-        $service = new FilesystemService();
-        $stream = $service->openFile($tempFilePath, 'w');
+        $stream = $this->service->openFile($tempFilePath, 'w');
         self::assertFalse($stream->isReadable());
         self::assertTrue($stream->isWritable());
         self::assertSame(0, $stream->getSize());
@@ -55,8 +62,7 @@ class FileSystemTest extends TestCase
     #[Depends("testFileCanBeWritten")]
     public function testFileCanBeRead(string $tempFilePath): void
     {
-        $service = new FilesystemService();
-        $stream = $service->openFile($tempFilePath, 'r');
+        $stream = $this->service->openFile($tempFilePath, 'r');
         self::assertFalse($stream->isWritable());
         self::assertTrue($stream->isReadable());
         self::assertTrue($stream->isSeekable());
