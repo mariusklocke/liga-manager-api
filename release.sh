6be2137cacf020c6f1276fd5a9ebf3c64c1d80c6
Marius Klocke <marius.klocke@gmail.com>
2024-12-09 19:21:09 +0100

    Improve release.sh

--- release.sh
+++ release.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
-set -e
+set -eu
 
+date=$(date +%Y-%m-%d)
 echo "Please enter a version number (MAJOR.MINOR.PATCH)"
 read -r version
-sed -i "/## NEXT/ a \\\n## $version - $(date +%Y-%m-%d)" CHANGELOG.md
-echo "Changelog has been modified. These are the first 15 lines:"
-head -n 15 CHANGELOG.md
+if grep "## $version - $date" CHANGELOG.md > /dev/null; then
+  echo "Changelog is valid: Contains supplied version and current date"
+else
+  echo "Changelog is invalid: Failed to find version and current date"
+  exit 1
+fi
+log_items=$(git diff -U0 CHANGELOG.md | grep "^+\*" | cut -c 2-)
+echo "These are changelog items found for $version"
+echo "$log_items"
 read -r -p "Are you sure you want to publish the release? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY])
@@ -19,8 +26,7 @@ case "$response" in
         exit
         ;;
     *)
-        git checkout CHANGELOG.md
-        echo "Release aborted. Changelog was reverted."
+        echo "Release aborted. No changes have been made."
         exit
         ;;
 esac
