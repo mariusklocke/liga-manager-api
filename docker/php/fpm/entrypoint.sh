79ee1ddf66028db597a0c6e131ed56e46a4a3653
Marius Klocke <marius.klocke@gmail.com>
2024-12-22 18:55:23 +0100

    Build FPM image against PostgreSQL

--- docker/php/fpm/entrypoint.sh
+++ docker/php/fpm/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$1" = "php-fpm" ]; then
     chown www-data:www-data /var/www/logos
     lima orm:generate-proxies --quiet
-    lima migrations:migrate -n --quiet
+    lima app:db:migrate --quiet
 fi
 
 exec "$@"
