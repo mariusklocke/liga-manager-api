d0a324a08323b173aa9bab2c0d70db37f748e11a
Marius Klocke <marius.klocke@gmail.com>
2025-05-09 19:28:08 +0200

    Restructure tests

--- tests/API/HealthTest.php
+++ tests/API/HealthTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace HexagonalPlayground\Tests\Health;
+namespace HexagonalPlayground\Tests\API;
 
 use HexagonalPlayground\Tests\Framework\HttpTest;
 
--- tests/API/MetricsTest.php
+++ tests/API/MetricsTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace HexagonalPlayground\Tests\Metrics;
+namespace HexagonalPlayground\Tests\API;
 
 use HexagonalPlayground\Tests\Framework\HttpTest;
 
