a768fb7eeb5609de3db994156c60470ef57b4548
Marius Klocke <marius.klocke@gmail.com>
2025-05-09 19:43:09 +0200

    Add IndexTest

---
+++ tests/API/IndexTest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace HexagonalPlayground\Tests\API;
+
+use HexagonalPlayground\Tests\Framework\HttpTest;
+
+class IndexTest extends HttpTest
+{
+    public function testIndexCanBeFetched(): void
+    {
+        $request = $this->createRequest('GET', '/api');
+
+        $response = $this->client->sendRequest($request);
+        self::assertSame(200, $response->getStatusCode());
+
+        $payload = $this->parser->parse($response);
+        self::assertIsObject($payload);
+
+        self::assertObjectHasProperty('limits', $payload);
+        self::assertIsObject($payload->limits);
+        self::assertObjectHasProperty('logos', $payload->limits);
+        self::assertIsObject($payload->limits->logos);
+        self::assertObjectHasProperty('size', $payload->limits->logos);
+        self::assertIsString($payload->limits->logos->size);
+        self::assertGreaterThan(0, strlen($payload->limits->logos->size));
+        self::assertObjectHasProperty('types', $payload->limits->logos);
+        self::assertIsArray($payload->limits->logos->types);
+        self::assertGreaterThan(0, count($payload->limits->logos->types));
+        self::assertObjectHasProperty('requests', $payload->limits);
+        self::assertIsString($payload->limits->requests);
+        self::assertGreaterThan(0, strlen($payload->limits->requests));
+        self::assertObjectHasProperty('version', $payload);
+        self::assertIsString($payload->version);
+        self::assertGreaterThan(0, strlen($payload->version));
+    }
+}
