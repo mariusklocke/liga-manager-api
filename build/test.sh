956824dd4cb5d25d6b6c4e92ef48929c5549904c
Marius Klocke <marius.klocke@gmail.com>
2025-05-23 17:41:26 +0200

    Add openapi validation to build/test.sh

--- build/test.sh
+++ build/test.sh
@@ -21,6 +21,7 @@ fi
 docker compose up --detach --quiet-pull
 docker compose exec php composer install --no-cache --no-progress
 docker compose exec php deptrac analyse --config-file config/deptrac.yaml --no-progress
+docker compose exec php php-openapi validate openapi.yml
 docker compose exec php phpunit -c config/phpunit.xml --testdox --display-deprecations --display-warnings
 docker compose exec php php-ext-configure enable xdebug
 docker compose exec php phpunit -c config/phpunit.xml --testdox --coverage-clover coverage.xml -d error_log=php-errors.log
