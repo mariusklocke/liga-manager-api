f738a6363d3a013dfdffa1521c009283c7b7648d
Marius Klocke <marius.klocke@gmail.com>
2025-05-25 18:51:23 +0200

    Fix pages artifact issue

--- build/docs.sh
+++ build/docs.sh
@@ -7,7 +7,6 @@ docker cp ${TEMP_CONTAINER}:/usr/share/nginx/html ./docs
 docker rm ${TEMP_CONTAINER}
 
 cp openapi.yml ./docs/openapi.yaml
-chmod 644 ./docs/*
 sed -i 's/%PAGE_TITLE%/Liga-Manager API docs/' ./docs/index.html
 sed -i 's/%BASE_PATH%//' ./docs/index.html
 sed -i 's/%PAGE_FAVICON%/favicon.png/' ./docs/index.html
