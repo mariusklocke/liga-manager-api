310ea16b99564267efc89334a630f59d1a951e41
Marius Klocke <marius.klocke@gmail.com>
2025-05-25 18:56:22 +0200

    Fix path issue

--- build/docs.sh
+++ build/docs.sh
@@ -10,5 +10,5 @@ cp openapi.yml ./docs/openapi.yml
 sed -i 's/%PAGE_TITLE%/Liga-Manager API docs/' ./docs/index.html
 sed -i 's/%BASE_PATH%//' ./docs/index.html
 sed -i 's/%PAGE_FAVICON%/favicon.png/' ./docs/index.html
-sed -i 's/%SPEC_URL%/\/openapi.yml/' ./docs/index.html
+sed -i 's/%SPEC_URL%/openapi.yml/' ./docs/index.html
 sed -i 's/%REDOC_OPTIONS%//' ./docs/index.html
