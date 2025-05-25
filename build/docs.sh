721cf675d8f58c3d111006c888b77042e9348d8e
Marius Klocke <marius.klocke@gmail.com>
2025-05-25 18:53:42 +0200

    Fix file extension

--- build/docs.sh
+++ build/docs.sh
@@ -6,9 +6,9 @@ TEMP_CONTAINER=$(docker create redocly/redoc)
 docker cp ${TEMP_CONTAINER}:/usr/share/nginx/html ./docs
 docker rm ${TEMP_CONTAINER}
 
-cp openapi.yml ./docs/openapi.yaml
+cp openapi.yml ./docs/openapi.yml
 sed -i 's/%PAGE_TITLE%/Liga-Manager API docs/' ./docs/index.html
 sed -i 's/%BASE_PATH%//' ./docs/index.html
 sed -i 's/%PAGE_FAVICON%/favicon.png/' ./docs/index.html
-sed -i 's/%SPEC_URL%/\/openapi.yaml/' ./docs/index.html
+sed -i 's/%SPEC_URL%/\/openapi.yml/' ./docs/index.html
 sed -i 's/%REDOC_OPTIONS%//' ./docs/index.html
