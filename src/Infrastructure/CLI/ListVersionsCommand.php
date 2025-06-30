c32a3e8e725f722b583ba29292367ce759d9b37f
Marius Klocke <marius.klocke@gmail.com>
2025-06-30 23:21:55 +0200

    Improve ListVersionsCommand

--- src/Infrastructure/CLI/ListVersionsCommand.php
+++ src/Infrastructure/CLI/ListVersionsCommand.php
@@ -3,6 +3,7 @@
 namespace HexagonalPlayground\Infrastructure\CLI;
 
 use Doctrine\DBAL\Connection;
+use Iterator;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Redis;
@@ -18,16 +19,24 @@ class ListVersionsCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $headers = ['Component', 'Version'];
-        $rows = [
-            ['Application', $this->container->get('app.version')],
-            ['Database', $this->container->get(Connection::class)->getServerVersion()],
-            ['Operating System', php_uname('s') . ' ' . php_uname('r')],
-            ['PHP', PHP_VERSION],
-            ['Redis', $this->container->get(Redis::class)->info('server')['redis_version']]
-        ];
+        $rows = [];
+
+        foreach ($this->getVersions() as $component => $version) {
+            $rows[] = [$component, $version];
+        }
 
         $this->getStyledIO($input, $output)->table($headers, $rows);
 
         return 0;
+    }
+
+    private function getVersions(): Iterator
+    {
+        yield 'Application' => $this->container->get('app.version');
+        yield 'Database' => $this->container->get(Connection::class)->getServerVersion();
+        yield php_uname('s') => php_uname('r');
+        yield 'OpenSSL' => explode(' ', OPENSSL_VERSION_TEXT)[1];
+        yield 'PHP' => PHP_VERSION;
+        yield 'Redis' => $this->container->get(Redis::class)->info('server')['redis_version'];
     }
 }
