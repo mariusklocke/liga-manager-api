fadc3a8780d56b700f0c3014ffa1bdfe3318124e
Marius Klocke <marius.klocke@gmail.com>
2026-03-25 23:46:53 +0100

    Fix API docs

--- build/docs.sh
+++ build/docs.sh
@@ -6,10 +6,26 @@ TEMP_CONTAINER=$(docker create redocly/redoc)
 docker cp "${TEMP_CONTAINER}:/usr/share/nginx/html" ./docs
 docker rm "${TEMP_CONTAINER}"
 
+cat > ./docs/index.html <<EOL
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Liga-Manager API docs</title>
+    <link rel="icon" href="favicon.png" />
+    <link rel="stylesheet" href="styles.css" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700"
+      rel="stylesheet"
+    />
+  </head>
+
+  <body>
+    <redoc spec-url="openapi.yml" type-of-usage="docker"></redoc>
+    <script type="module" src="redoc.standalone.js"></script>
+  </body>
+</html>
+EOL
 cp openapi.yml ./docs/openapi.yml
-sed -i 's/${PAGE_TITLE}/Liga-Manager API docs/' ./docs/index.tpl.html
-sed -i 's/${PAGE_FAVICON}/favicon.png/' ./docs/index.tpl.html
-sed -i 's/${SPEC_URL}/openapi.yml/' ./docs/index.tpl.html
-sed -i 's/${REDOC_OPTIONS}//' ./docs/index.tpl.html
-cp ./docs/index.tpl.html ./docs/index.html
-rm ./docs/index.tpl.html
+rm ./docs/index.tpl.html ./docs/index.prefix.tpl.html
