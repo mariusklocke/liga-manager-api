faedaa27803c7db894fca17929798608904de847
Marius Klocke <marius.klocke@gmail.com>
2026-03-25 23:20:52 +0100

    Fix API docs

--- build/docs.sh
+++ build/docs.sh
@@ -7,8 +7,9 @@ docker cp "${TEMP_CONTAINER}:/usr/share/nginx/html" ./docs
 docker rm "${TEMP_CONTAINER}"
 
 cp openapi.yml ./docs/openapi.yml
-sed -i 's/%PAGE_TITLE%/Liga-Manager API docs/' ./docs/index.html
-sed -i 's/%BASE_PATH%//' ./docs/index.html
-sed -i 's/%PAGE_FAVICON%/favicon.png/' ./docs/index.html
-sed -i 's/%SPEC_URL%/openapi.yml/' ./docs/index.html
-sed -i 's/%REDOC_OPTIONS%//' ./docs/index.html
+sed -i 's/${PAGE_TITLE}/Liga-Manager API docs/' ./docs/index.tpl.html
+sed -i 's/${PAGE_FAVICON}/favicon.png/' ./docs/index.tpl.html
+sed -i 's/${SPEC_URL}/openapi.yml/' ./docs/index.tpl.html
+sed -i 's/${REDOC_OPTIONS}//' ./docs/index.tpl.html
+cp ./docs/index.tpl.html ./docs/index.html
+rm ./docs/index.tpl.html
