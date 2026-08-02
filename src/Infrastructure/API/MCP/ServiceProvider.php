26d2b086b8569fce8340b28aae26051363533419
Marius Klocke <marius.klocke@gmail.com>
2026-08-02 12:43:59 +0200

    Fix class interface

--- src/Infrastructure/API/MCP/ServiceProvider.php
+++ src/Infrastructure/API/MCP/ServiceProvider.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace HexagonalPlayground\Infrastructure\API\MCP;
 
 use DI;
+use HexagonalPlayground\Application\ServiceProviderInterface;
 use HexagonalPlayground\Infrastructure\API\MCP\Tool\ListTeams;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 
-class ServiceProvider
+class ServiceProvider implements ServiceProviderInterface
 {
-    public function register(): array
+    public function getDefinitions(): array
     {
         return [
             Controller::class => DI\factory(function (ContainerInterface $container) {
